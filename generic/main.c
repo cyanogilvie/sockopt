d815b312ce65984037beb60bb7ba3887dc23a6c5
Cyan Ogilvie <cyan@tirion.(none)>
2009-03-30 14:17:24 +0200

    Fixes

--- generic/main.c
+++ generic/main.c
@@ -11,9 +11,11 @@ static int glue_setsockopt(cdata, interp, objc, objv) //<<<
 	int				objc;
 	Tcl_Obj* CONST	objv[];
 {
-	int					s, optval, res;
-	Tcl_Channel			chan;
-	Tcl_ChannelType*	chantype;
+	ClientData				st;
+	int						s, optval, res, tmpres, tmpoptval;
+	Tcl_Channel				chan;
+	const Tcl_ChannelType*	chantype;
+	socklen_t				optlen = sizeof(optval);
 
 	static CONST char* levels[] = {
 		"SOL_SOCKET",
@@ -24,7 +26,11 @@ static int glue_setsockopt(cdata, interp, objc, objv) //<<<
 	int level_map[] = {
 		SOL_SOCKET,
 		SOL_TCP,
+#ifdef IPPROTO_TCP
 		IPPROTO_TCP
+#else
+		SOL_TCP
+#endif
 	};
 	int level_index;
 
@@ -55,8 +61,10 @@ static int glue_setsockopt(cdata, interp, objc, objv) //<<<
 	if (strcmp(Tcl_ChannelName(chantype), "tcp") != 0)
 		THROW_ERROR("Not a TCP channel");
 
-	if (Tcl_GetChannelHandle(chan, TCL_WRITABLE, (ClientData*)&s) != TCL_OK)
+	if (Tcl_GetChannelHandle(chan, TCL_WRITABLE, &st) != TCL_OK)
 		THROW_ERROR("Could not retrieve OS socket handle");
+	s = (long int)st;	// This is not cool
+	//fprintf(stderr, "setsockopt chan is \"%s\" s is %d\n", Tcl_GetString(objv[1]), s);
 
 	TEST_OK(Tcl_GetIndexFromObj(interp, objv[2], levels, "level",
 				TCL_EXACT, &level_index));
@@ -66,13 +74,115 @@ static int glue_setsockopt(cdata, interp, objc, objv) //<<<
 
 	TEST_OK(Tcl_GetIntFromObj(interp, objv[4], &optval));
 
-	fprintf(stderr, "setsockopt(%d, %d, %d, %d, %d);\n",
-			s, level_map[level_index], optname_map[optname_index],
-			optval, sizeof(int));
+	tmpres = getsockopt(s, level_map[level_index], optname_map[optname_index],
+			&tmpoptval, &optlen);
+	//fprintf(stderr, "before %d = getsockopt(%d, %d, %d, &tmpoptval(%d), &optlen(%d))\n",
+	//		tmpres, s, level_map[level_index], optname_map[optname_index],
+	//		tmpoptval, optlen);
+
+	//fprintf(stderr, "setsockopt(%d, %d, %d, %d, %ld);\n",
+	//		s, level_map[level_index], optname_map[optname_index],
+	//		optval, sizeof(int));
 	res = setsockopt(s, level_map[level_index], optname_map[optname_index],
 			(const void*)&optval, sizeof(int));
 
+	tmpres = getsockopt(s, level_map[level_index], optname_map[optname_index],
+			&tmpoptval, &optlen);
+	//fprintf(stderr, "after %d = getsockopt(%d, %d, %d, &tmpoptval(%d), &optlen(%d))\n",
+	//		tmpres, s, level_map[level_index], optname_map[optname_index],
+	//		tmpoptval, optlen);
+
 	if (res == 0) {
+		return TCL_OK;
+	} else {
+		Tcl_SetObjResult(interp, Tcl_NewStringObj(Tcl_ErrnoMsg(Tcl_GetErrno()), -1));
+		return TCL_ERROR;
+	}
+}
+
+//>>>
+static int glue_getsockopt(cdata, interp, objc, objv) //<<<
+	ClientData		cdata;
+	Tcl_Interp*		interp;
+	int				objc;
+	Tcl_Obj* CONST	objv[];
+{
+	ClientData				st;
+	int						s, optval, res;
+	int						tmpoptval, tmpres;
+	Tcl_Channel				chan;
+	const Tcl_ChannelType*	chantype;
+	socklen_t				optlen = sizeof(optval);
+	Tcl_WideInt				staging_optval;
+
+	static CONST char* levels[] = {
+		"SOL_SOCKET",
+		"SOL_TCP",
+		"IPPROTO_TCP",
+		(char *)NULL
+	};
+	int level_map[] = {
+		SOL_SOCKET,
+		SOL_TCP,
+#ifdef IPPROTO_TCP
+		IPPROTO_TCP
+#else
+		SOL_TCP
+#endif
+	};
+	int level_index;
+
+	static CONST char* optnames[] = {
+		"SO_KEEPALIVE",
+		"TCP_KEEPCNT",
+		"TCP_KEEPIDLE",
+		"TCP_KEEPINTVL",
+		"TCP_NODELAY",
+		(char *)NULL
+	};
+	int optname_map[] = {
+		SO_KEEPALIVE,
+		TCP_KEEPCNT,
+		TCP_KEEPIDLE,
+		TCP_KEEPINTVL,
+		TCP_NODELAY
+	};
+	int optname_index;
+
+	CHECK_ARGS(3, "socket_handle level optname");
+
+	chan = Tcl_GetChannel(interp, Tcl_GetString(objv[1]), NULL);
+	if (chan == NULL)
+		THROW_ERROR("No such channel: ", Tcl_GetString(objv[1]));
+
+	chantype = Tcl_GetChannelType(chan);
+	if (strcmp(Tcl_ChannelName(chantype), "tcp") != 0)
+		THROW_ERROR("Not a TCP channel");
+
+	if (Tcl_GetChannelHandle(chan, TCL_WRITABLE, &st) != TCL_OK)
+		THROW_ERROR("Could not retrieve OS socket handle");
+	s = (long int)st;	// This is not cool
+	//fprintf(stderr, "getsockopt chan is \"%s\" s is %d\n", Tcl_GetString(objv[1]), s);
+
+	TEST_OK(Tcl_GetIndexFromObj(interp, objv[2], levels, "level",
+				TCL_EXACT, &level_index));
+
+	TEST_OK(Tcl_GetIndexFromObj(interp, objv[3], optnames, "optname",
+				TCL_EXACT, &optname_index));
+
+	res = getsockopt(s, level_map[level_index], optname_map[optname_index],
+			&optval, &optlen);
+	//fprintf(stderr, "--- before %d = getsockopt(%d, %d, %d, &optval(%d), &optlen(%d))\n",
+	//		res, s, level_map[level_index], optname_map[optname_index],
+	//		optval, optlen);
+
+	tmpres = getsockopt(s, SOL_SOCKET, SO_KEEPALIVE, &tmpoptval, &optlen);
+	//fprintf(stderr, "--- %d = getsockopt(%d, SOL_SOCKET, SO_KEEPALIVE): %d\n",
+	//		tmpres, s, tmpoptval);
+
+	if (res == 0) {
+		staging_optval = optval;
+		Tcl_SetObjResult(interp, Tcl_NewWideIntObj(staging_optval));
 		return TCL_OK;
 	} else {
 		Tcl_SetObjResult(interp, Tcl_NewStringObj(Tcl_ErrnoMsg(Tcl_GetErrno()), -1));
@@ -87,10 +197,11 @@ static int glue_dumpsockopt(cdata, interp, objc, objv) //<<<
 	int				objc;
 	Tcl_Obj* CONST	objv[];
 {
-	int					s, optval, res;
-	socklen_t			optval_len;
-	Tcl_Channel			chan;
-	Tcl_ChannelType*	chantype;
+	ClientData				st;
+	int						s, optval, res;
+	Tcl_Channel				chan;
+	const Tcl_ChannelType*	chantype;
+	socklen_t				optlen = sizeof(optval);
 
 	CHECK_ARGS(1, "socket_handle");
 
@@ -102,28 +213,30 @@ static int glue_dumpsockopt(cdata, interp, objc, objv) //<<<
 	if (strcmp(Tcl_ChannelName(chantype), "tcp") != 0)
 		THROW_ERROR("Not a TCP channel");
 
-	if (Tcl_GetChannelHandle(chan, TCL_WRITABLE, (ClientData*)&s) != TCL_OK)
+	if (Tcl_GetChannelHandle(chan, TCL_WRITABLE, &st) != TCL_OK)
 		THROW_ERROR("Could not retrieve OS socket handle");
+	s = (long int)st;	// This is not cool
+	fprintf(stderr, "dumpsockopt chan is \"%s\" s is %d\n", Tcl_GetString(objv[1]), s);
 
-	res = getsockopt(s, SOL_SOCKET, SO_KEEPALIVE, (void*)&optval, &optval_len);
-	fprintf(stderr, "%d = getsockopt(s, SOL_SOCKET, SO_KEEPALIVE): %d\n",
-			res, optval);
+	res = getsockopt(s, SOL_SOCKET, SO_KEEPALIVE, &optval, &optlen);
+	fprintf(stderr, "--- %d = getsockopt(%d, SOL_SOCKET, SO_KEEPALIVE): %d\n",
+			res, s, optval);
 
-	res = getsockopt(s, SOL_TCP, TCP_KEEPIDLE, (void*)&optval, &optval_len);
-	fprintf(stderr, "%d = getsockopt(s, SOL_TCP, TCP_KEEPIDLE): %d\n",
-			res, optval);
+	res = getsockopt(s, SOL_TCP, TCP_KEEPIDLE, &optval, &optlen);
+	fprintf(stderr, "--- %d = getsockopt(%d, SOL_TCP, TCP_KEEPIDLE): %d\n",
+			res, s, optval);
 
-	res = getsockopt(s, SOL_TCP, TCP_KEEPCNT, (void*)&optval, &optval_len);
-	fprintf(stderr, "%d = getsockopt(s, SOL_TCP, TCP_KEEPCNT): %d\n",
-			res, optval);
+	res = getsockopt(s, SOL_TCP, TCP_KEEPCNT, &optval, &optlen);
+	fprintf(stderr, "--- %d = getsockopt(%d, SOL_TCP, TCP_KEEPCNT): %d\n",
+			res, s, optval);
 
-	res = getsockopt(s, SOL_TCP, TCP_KEEPINTVL, (void*)&optval, &optval_len);
-	fprintf(stderr, "%d = getsockopt(s, SOL_TCP, TCP_KEEPINTVL): %d\n",
-			res, optval);
+	res = getsockopt(s, SOL_TCP, TCP_KEEPINTVL, &optval, &optlen);
+	fprintf(stderr, "--- %d = getsockopt(%d, SOL_TCP, TCP_KEEPINTVL): %d\n",
+			res, s, optval);
 
-	res = getsockopt(s, IPPROTO_TCP, TCP_NODELAY, (void*)&optval, &optval_len);
-	fprintf(stderr, "%d = getsockopt(s, SOL_TCP, TCP_NODELAY): %d\n",
-			res, optval);
+	res = getsockopt(s, IPPROTO_TCP, TCP_NODELAY, &optval, &optlen);
+	fprintf(stderr, "--- %d = getsockopt(%d, IPPROTO_TCP, TCP_NODELAY): %d\n",
+			res, s, optval);
 
 	return TCL_OK;
 }
@@ -135,6 +248,7 @@ int Sockopt_Init(Tcl_Interp *interp) //<<<
 		return TCL_ERROR;
 
 	NEW_CMD("sockopt::setsockopt", glue_setsockopt);
+	NEW_CMD("sockopt::getsockopt", glue_getsockopt);
 	NEW_CMD("sockopt::dumpsockopt", glue_dumpsockopt);
 
 	TEST_OK(Tcl_PkgProvide(interp, PACKAGE_NAME, PACKAGE_VERSION));
